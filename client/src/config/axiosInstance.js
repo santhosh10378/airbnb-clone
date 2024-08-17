f15a4b66838f342e210a513280e35cd773afcc4d
Santhosh Kumar <santhosh10378@gmail.com>
2024-08-18 01:58:15 +0530

    changed axiosInstance

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-import { useAuth } from "../context/AuthContext";
 
 const backendURL = import.meta.env.VITE_BACKEND_URL;
 const baseURL = `${backendURL}/api`;
-const { token } = useAuth();
+const authToken = localStorage.getItem("authToken");
 
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${authToken}`,
   },
 });
